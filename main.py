bf982c7038ef77588c922122cd8714632ced5207
henrichhegedus <36788225+henrichhegedus@users.noreply.github.com>
2018-03-07 18:51:59 +0100

    Popisky pridane

--- main.py
+++ main.py
@@ -8,39 +8,44 @@ pump=Pin(0, Pin.OUT)
 zatvorit=Pin(4, Pin.OUT)
 otvorit=Pin(5, Pin.OUT)
 
+# Definovanie funkcie vetrania. Funkcia otvori ventil ak je vlhkost vacsia alebo rovna 70%. Otvori vetranie na 20s a pusti ventilator
+# ventilator sa vypne tesne pred tym ako sa zatvori vetranie
 def vetranie(value):
   if value>=70:
     otvorit.on()
-    utime.sleep(2)
+    utime.sleep(0.5)
     otvorit.off()
-    utime.sleep(1)
     fan.on()
     utime.sleep(20)
     fan.off()
-    utime.sleep(1)
     zatvorit.on()
-    utime.sleep(2)
+    utime.sleep(0.5)
     zatvorit.off()
     
-
+# Ked je tato funkcia vyvolana tak vrati aktualny cas
 def aktualny_cas():  
   return(utime.mktime(utime.localtime()))  
-  
+
+# Funkcia zavlaha bere jeden argument vlhkost a podla toho ci je menej alebo viac ako 45 zapne alebo necha vypnute zalievanie.
+# Ak zalievanie zapne tak bude pustene na 10s
 def zavlaha(vlhkost):
   if vlhkost<=45:
     pump.on()
     utime.sleep(10)
     pump.off()
   
-  
-
+# Funkcia vlhkost_a_teplota zapise do textoveho suboru data.txt aktualnu vlhkost a teplotu vzduchu aktualny cas a cislo merania.
+# Navyse tato funkcia vrati vlhkost ktora je pouzita na urcenie toho ci sa bude vetrat alebo nie
 def vlhkost_a_teplota():
   sensor.measure()
   data=open('data.txt',"a")
   data.write(str({meranie:[sensor.humidity(),sensor.temperature(),aktualny_cas()]}))
   data.close()
   return(sensor.humidity())
- 
+# Funkcia vlhkost_pody odmeria 10x za sebou vlhkost pody kazde meranie zapise do listu list potom z poslednych piatich merani
+# je vypocitany priemer ktoreho hodnota je ulozena do premennej average. Po 10 meraniach sa privod prudu do senzora vypne aby
+# nekorodoval zbytocne. Priemer nameranych hodnot je zapisany do textoveho suboru data.txt aj s cislom merania a aktualnym casom
+# Taktiez funkcia vrati vlhkost pody v percentach ktora je vyuzivana na urcenie toho ci sa bude zavlazovat alebo nie
 def vlhkost_pody():
   data=open('data.txt',"a")
   list=[]
@@ -55,7 +60,7 @@ def vlhkost_pody():
   data.close()
   print (average)
   return (average*100)
-
+# Tento loop zaruci to ze kazdu hodinu prebehne jedno meranie a sklenik skontroluje ci nie je potrebne zalievat alebo vetrat
 while 1==1:
   meranie+=1
   vetranie(vlhkost_a_teplota())
